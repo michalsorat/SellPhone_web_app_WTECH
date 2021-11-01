017eab7ed5ebb61589b51fcc0c63170dd1e882c2
Michal Sorát <michalsorat@gmail.com>
2021-11-02 00:02:20 +0100

    homePage slider created, navbar finished

--- resources/views/homePage.blade.php
+++ resources/views/homePage.blade.php
@@ -1,7 +1,72 @@
 @extends('layouts.app')
 
 @section('content')
-    <div>
-        homepage
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+
+{{--    <div class="row md-2 mx-3">--}}
+{{--        <div class="col-md-3 col-sm-6">--}}
+{{--            <div class="jumbotron">Box1</div>--}}
+{{--        </div>--}}
+{{--        <div class="col-md-3 col-sm-6">--}}
+{{--            <div class="jumbotron">Box2</div>--}}
+{{--        </div>--}}
+{{--        <div class="col-md-3 col-sm-6">--}}
+{{--            <div class="jumbotron">Box3</div>--}}
+{{--        </div>--}}
+{{--        <div class="col-md-3 col-sm-6">--}}
+{{--            <div class="jumbotron">Box4</div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+
+    <div class="row m-3">
+        <div class="col-lg-3 col-6">
+            <div class="jumbotron d-flex my-auto">
+                <img src="https://cdn-icons.flaticon.com/png/512/969/premium/969088.png?token=exp=1635803620~hmac=e6c3f7cfcd1db4ce67a06dc8acf57223"/>
+                <h6>Viac ako 20 000 spokojných zákazníkov</h6>
+            </div>
+        </div>
+        <div class="col-lg-3 col-6">
+            <div class="jumbotron d-flex my-auto">
+                <img src="https://cdn-icons-png.flaticon.com/512/1428/1428459.png"/>
+                <h6>Bezplatné vrátenie tovaru do 30 dní</h6>
+            </div>
+        </div>
+        <div class="col-lg-3 col-6">
+            <div class="jumbotron d-flex my-auto">
+                <img src="https://cdn-icons.flaticon.com/png/512/4014/premium/4014357.png?token=exp=1635803044~hmac=ac94c3c9a874e6919c3f9efba2a0c7a1"/>
+                <h6>Záruka dodania nového zapečateného tovaru</h6>
+            </div>
+        </div>
+        <div class="col-lg-3 col-6">
+            <div class="jumbotron d-flex my-auto">
+                <img src="https://cdn-icons-png.flaticon.com/512/869/869121.png"/>
+                <h6>Expresné doručenie do 24 hodín</h6>
+            </div>
+        </div>
     </div>
 @endsection
--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,37 +1,76 @@
-{{--<div class="row">--}}
-{{--    <div class="col-5">--}}
-{{--        <h1>SellPhone.sk</h1>--}}
-{{--    </div>--}}
-{{--    <div class="col-5 mt-2">--}}
-{{--        <input class="form-control" type="search" placeholder="Search" aria-label="Search">--}}
+{{--<div class="d-flex justify-content-between">--}}
+{{--    <h1>SellPhone.sk</h1>--}}
+{{--    <div>--}}
+{{--        <input id="search" type="search" placeholder="Search" aria-label="Search" size="10" autocomplete="off">--}}
 {{--    </div>--}}
 {{--    <div class="col-2 d-flex justify-content-end mt-3" id="nav-icons">--}}
 {{--        <i class="far fa-user"></i>--}}
 {{--        <i class="fas fa-shopping-cart"></i>--}}
 {{--    </div>--}}
 {{--</div>--}}
+{{--<div class="row">--}}
+{{--    <div class="col-4">--}}
+{{--        <h1>SellPhone.sk</h1>--}}
+{{--    </div>--}}
+{{--    <div class="col-4 mt-2">--}}
+{{--        <form class="d-flex ">--}}
+{{--            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">--}}
+{{--            </span>--}}
+{{--            <span class="input-group-append">--}}
+{{--                    <button class="btn btn-outline-secondary" type="button">--}}
+{{--                        <i class="fa fa-search"></i>--}}
+{{--                    </button>--}}
+{{--                </span>--}}
+{{--        </form>--}}
+{{--    </div>--}}
+{{--    <div class="col-4 d-flex justify-content-end mt-3" id="nav-icons">--}}
+{{--        <i class="far fa-user"></i>--}}
+{{--        <i class="fas fa-shopping-cart"></i>--}}
+{{--    </div>--}}
+{{--</div>--}}
 <div class="d-flex justify-content-between">
-    <h1>SellPhone.sk</h1>
+    <h1 id="main-header">SellPhone.sk</h1>
     <div>
-        <input type="search" placeholder="Search" aria-label="Search">
+        <form class="d-sm-flex d-none my-2">
+            <input class="form-control" type="search" placeholder="Search" aria-label="Search" size="25">
+            <span class="input-group-append">
+                <button class="btn btn-outline-secondary" type="button">
+                    <i class="fa fa-search"></i>
+                </button>
+            </span>
+        </form>
     </div>
     <div class="col-2 d-flex justify-content-end mt-3" id="nav-icons">
         <i class="far fa-user"></i>
         <i class="fas fa-shopping-cart"></i>
     </div>
 </div>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
     <div class="container-fluid">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main_nav"  aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="main_nav">
-            <ul class="nav justify-content-center">
-                <li class="nav-item"><a class="nav-link active" href="#">Home </a></li>
-                <li class="nav-item"><a class="nav-link" href="#"> About </a></li>
-                <li class="nav-item"><a class="nav-link" href="#"> Services </a></li>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav w-100 nav-justified">
+                <li class="nav-item"><a id="test" class="nav-link" href="#">Apple</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Samsung</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Huawei</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Xiaomi</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">OnePlus</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">LG</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Príslušenstvo</a></li>
             </ul>
+        </div>
+        <div>
+            <form class="d-sm-none d-flex">
+                <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+                <span class="input-group-append">
+                <button class="btn btn-outline-secondary" type="button">
+                    <i class="fa fa-search"></i>
+                </button>
+            </span>
+            </form>
         </div>
     </div>
 </nav>
-{{--</div>--}}
+
